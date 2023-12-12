59feabe51c0949a6884779ea6eea4cca7f904b0d
Carlos Moraga <c.moraga.tradu@gmail.com>
2023-12-12 00:42:36 -0300

    Update projects sections

--- pages/index.vue
+++ pages/index.vue
@@ -118,7 +118,7 @@ setTimeout(() => {
                 proyectos utilizando Laravel para desarrollar el entorno de Backend y para Frontend el framework
                 Vue 3 y Nuxt 3. También he implementado en proyectos herramientas de trabajo como WebPay y Pinia para
                 almacenamiento, librerías de diseño
-                como Nuxt UI, Vuetify, Ant Design, así como tambien consumos de API Rest entre proyectos Backend y
+                como Nuxt UI, Vuetify, Ant Design, así como también consumos de API Rest entre proyectos Backend y
                 Frontend.</p>
             </div>
           </div>
@@ -155,7 +155,53 @@ setTimeout(() => {
           <h2 class="lg:text-5xl sm:text-3xl font-bold lg:px-20 sm:px-10 text-justify flex justify-center p-6">
             Proyectos
           </h2>
-          <div class="grid lg:grid-cols-4 sm:grid-col-1 gap-4 font-mono">
+          <div class="grid lg:grid-cols-4 sm:grid-col-1 gap-2 font-mono">
+            <div class="lg:col-start-1 lg:col-span-2 sm:col-start-1 sm:col-span-1">
+              <div class="grid justify-items-center">
+                <div
+                  class="p-5 w-80 flex flex-col items-center justify-center text-center rounded-md border-4 border-main-color shadow-xl transition transition border-opacity-0 hover:border-opacity-100 hover:bg-blue-200">
+                  <h3 class="lg:text-3xl sm:text-xl font-bold">To-Do-List-Api</h3>
+                  <div class="grid justify-items-center p-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 256 264">
+                      <path fill="#FF2D20"
+                        d="M255.856 59.62c.095.351.144.713.144 1.077v56.568c0 1.478-.79 2.843-2.073 3.578L206.45 148.18v54.18a4.135 4.135 0 0 1-2.062 3.579l-99.108 57.053c-.227.128-.474.21-.722.299c-.093.03-.18.087-.278.113a4.15 4.15 0 0 1-2.114 0c-.114-.03-.217-.093-.325-.134c-.227-.083-.464-.155-.68-.278L2.073 205.938A4.128 4.128 0 0 1 0 202.36V32.656c0-.372.052-.733.144-1.083c.031-.119.103-.227.145-.346c.077-.216.15-.438.263-.639c.077-.134.19-.242.283-.366c.119-.165.227-.335.366-.48c.119-.118.274-.206.408-.309c.15-.124.283-.258.453-.356h.005L51.613.551a4.135 4.135 0 0 1 4.125 0l49.546 28.526h.01c.165.104.305.232.454.351c.134.103.284.196.402.31c.145.149.248.32.371.484c.088.124.207.232.279.366c.118.206.185.423.268.64c.041.118.113.226.144.35c.095.351.144.714.145 1.078V138.65l41.286-23.773V60.692c0-.36.052-.727.145-1.072c.036-.124.103-.232.144-.35c.083-.217.155-.44.268-.64c.077-.134.19-.242.279-.366c.123-.165.226-.335.37-.48c.12-.118.269-.206.403-.309c.155-.124.289-.258.454-.356h.005l49.551-28.526a4.13 4.13 0 0 1 4.125 0l49.546 28.526c.175.103.309.232.464.35c.128.104.278.197.397.31c.144.15.247.32.37.485c.094.124.207.232.28.366c.118.2.185.423.267.64c.047.118.114.226.145.35m-8.115 55.258v-47.04l-17.339 9.981l-23.953 13.792v47.04l41.297-23.773zm-49.546 85.095V152.9l-23.562 13.457l-67.281 38.4v47.514zM8.259 39.796v160.177l90.833 52.294v-47.505L51.64 177.906l-.015-.01l-.02-.01c-.16-.093-.295-.227-.444-.34c-.13-.104-.279-.186-.392-.3l-.01-.015c-.134-.129-.227-.289-.34-.433c-.104-.14-.227-.258-.31-.402l-.005-.016c-.093-.154-.15-.34-.217-.515c-.067-.155-.154-.3-.196-.464v-.005c-.051-.196-.061-.403-.082-.604c-.02-.154-.062-.309-.062-.464V63.57L25.598 49.772l-17.339-9.97zM53.681 8.893L12.399 32.656l41.272 23.762L94.947 32.65L53.671 8.893zm21.468 148.298l23.948-13.786V39.796L81.76 49.778L57.805 63.569v103.608zM202.324 36.935l-41.276 23.762l41.276 23.763l41.271-23.768zm-4.13 54.676l-23.953-13.792l-17.338-9.981v47.04l23.948 13.787l17.344 9.986zm-94.977 106.006l60.543-34.564l30.264-17.272l-41.246-23.747l-47.489 27.34l-43.282 24.918z" />
+                    </svg>
+                  </div>
+                  <div class="bx pt-8 text-6xl text-main-color">
+                    <NuxtLink to="https://github.com/CarlosMoragaDev/To-Do-List-Api" target="_blank">
+                      <UButton class="text-main-color text-xl font-medium hover:text-red-600" label="Revisar"
+                        variant="link" />
+                    </NuxtLink>
+                  </div>
+                </div>
+              </div>
+              <div class="grid justify-items-center">
+              </div>
+            </div>
+            <div class="lg:col-start-3 lg:col-span-2 sm:col-start-1 sm:col-span-1">
+              <div class="grid justify-items-center">
+                <div
+                  class="p-5 w-80 flex flex-col items-center justify-center text-center rounded-md border-4 border-main-color shadow-xl transition transition border-opacity-0 hover:border-opacity-100 hover:bg-blue-200">
+                  <h3 class="lg:text-4xl sm:text-2xl font-bold">To-Do-List</h3>
+                  <div class="grid justify-items-center p-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 512 121">
+                      <path fill="#00DC82"
+                        d="M103.405 120.261h68.52c2.177 0 4.314-.555 6.2-1.61a12.213 12.213 0 0 0 4.536-4.403a11.754 11.754 0 0 0 1.66-6.013c0-2.111-.575-4.185-1.665-6.013l-46.016-77.31a12.215 12.215 0 0 0-4.536-4.402a12.69 12.69 0 0 0-6.198-1.61a12.69 12.69 0 0 0-6.198 1.61a12.214 12.214 0 0 0-4.537 4.402l-11.766 19.78l-23.004-38.68a12.22 12.22 0 0 0-4.539-4.401A12.7 12.7 0 0 0 69.663 0a12.7 12.7 0 0 0-6.199 1.61a12.22 12.22 0 0 0-4.538 4.402l-57.262 96.21A11.737 11.737 0 0 0 0 108.235c0 2.11.571 4.185 1.659 6.013a12.214 12.214 0 0 0 4.537 4.402a12.692 12.692 0 0 0 6.2 1.611h43.01c17.042 0 29.61-7.26 38.257-21.426l20.994-35.268l11.245-18.874l33.75 56.694h-44.995zm-48.7-18.893l-30.016-.007l44.994-75.588l22.45 37.794l-15.031 25.261c-5.743 9.191-12.267 12.54-22.397 12.54" />
+                      <path
+                        d="M215.04 119.13h20.119V74.115c0-12.881-1.342-25.762-1.342-25.762l1.186 2.641c1.871 4.088 5.77 12.297 10.349 20.149l27.494 47.985h21.594V20.044h-20.252l.002 45.862c.056 12.598 1.339 25.055 1.339 25.055l-1.014-2.296c-1.79-3.986-5.764-12.539-10.386-20.636l-27.63-47.985H215.04zm143.205-71.342v39.068c0 9.059-5.767 15.853-13.949 15.853c-7.644 0-13.01-6.228-13.01-14.863V47.788h-18.642v44.446c0 16.279 9.925 28.028 25.08 28.028c9.255 0 16.765-4.247 20.52-11.041v9.908h18.778V47.788zm73.38 34.255l22.669-34.255H433.77l-12.337 18.826l-12.342-18.826h-20.386l22.669 34.114l-24.411 37.227h19.986l14.616-21.94l14.484 21.94h19.852zm43.627-34.255h-13.011v16.42h13.011v29.017c0 16.562 10.059 25.904 25.618 25.904H512v-16.561h-8.582c-6.035 0-9.525-3.256-9.525-10.192V64.207H512v-16.42h-18.107v-21.09h-18.64z" />
+                    </svg>
+                  </div>
+                  <div class="bx pt-8 text-6xl text-main-color">
+                    <NuxtLink to="https://github.com/CarlosMoragaDev/To-Do-List" target="_blank">
+                      <UButton class="text-main-color text-xl font-medium hover:text-red-600" label="Revisar"
+                        variant="link" />
+                    </NuxtLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-4 grid lg:grid-cols-4 sm:grid-col-1 gap-2 font-mono">
             <div class="lg:col-start-1 lg:col-span-2 sm:col-start-1 sm:col-span-1">
               <div class="grid justify-items-center">
                 <div
@@ -200,13 +246,11 @@ setTimeout(() => {
               </div>
             </div>
           </div>
-          <div>
-          </div>
           <div class="p-5">
             <div class="grid justify-items-center">
               <div class="p-5 w-80% flex flex-col items-center justify-center text-center rounded-md border-4 border-main-color shadow-xl
                  transition border-opacity-0 hover:border-opacity-100 hover:bg-blue-200">
-                 
+
                 <h3 class="lg:text-4xl sm:text-2xl font-bold pb-4">Pokedex</h3>
                 <UCard :ui="{
                   base: 'overflow-hidden', header: { background: 'bg-red-600' },
@@ -233,7 +277,7 @@ setTimeout(() => {
                         }}
                         </div>
                         <div class="lg:text-2xl sm:text-xl font-light text-justify capitalize p-5">{{ 'Nombre: ' +
-                           dataName   }}</div>
+                          dataName }}</div>
                         <div v-for="(type, index) in dataTypes" :key="index">
                           <div class="lg:text-2xl sm:text-xl font-light text-justify capitalize p-5">{{
                             'Tipo: ' + type?.type?.name }}
